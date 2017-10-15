1ee93676cc90f4d0d4e2a322b00b04116fd7abc0
everget <alex.or161@mail.ru>
2017-10-15 15:38:50 +0300

    STATIC: Update static files

--- src/index.js
+++ src/index.js
@@ -17,8 +17,12 @@ function callback() {
   document.getElementsByClassName('js-item-result-browser-version')[0].textContent = rowser.summary.version || 'unknown';
   document.getElementsByClassName('js-item-result-engine')[0].textContent = rowser.summary.engine || 'unknown';
   document.getElementsByClassName('js-item-result-engine-version')[0].textContent = rowser.summary.engineversion || 'unknown';
-  document.getElementsByClassName('js-item-result-os')[0].textContent = rowser.summary.os || 'unknown';
+  document.getElementsByClassName('js-item-result-os')[0].textContent = rowser.summary.osname || 'unknown';
   document.getElementsByClassName('js-item-result-os-version')[0].textContent = rowser.summary.osversion || 'unknown';
+
+  document.getElementsByClassName('js-item-result-device')[0].textContent = rowser.summary.device || 'unknown';
+
+  document.getElementsByClassName('js-item-result-device-type')[0].textContent = (rowser.summary.mobile && 'Mobile' || rowser.summary.tablet && 'Tablet' || rowser.summary.tv && 'TV' || rowser.summary.gameconsole && 'Game Console') || 'unknown';
 }
 
 if (document.readyState === 'complete' || (document.readyState !== 'loading' && !document.documentElement.doScroll)) {
